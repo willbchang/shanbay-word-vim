a20527ab21944b69b6df68f69a116d758dbe7693
Will B Chang <will.b.chang@gmail.com>
2021-03-04 10:06:34 +0800

    docs: update comments

--- userscript.js
+++ userscript.js
@@ -2,7 +2,7 @@
 // @name         Shanbay JK
 // @namespace    https://github.com/willbchang/shanbay-jk
 // @version      1.0
-// @description  Make j work like 1 and d, make k work like 2.
+// @description  在扇贝单词网页版上让 j 拥有 1 跟 d 的功能，k 拥有 2 的功能
 // @author       Will B Chang
 // @match        https://web.shanbay.com/wordsweb/*
 // @grant        GPL-3.0
@@ -11,6 +11,7 @@
 (function () {
     'use strict';
 
+    // 模拟点击事件
     function click(element) {
         const event = document.createEvent('Events');
         event.initEvent('click', true, false);
@@ -18,11 +19,14 @@
     }
 
     document.addEventListener('keypress', ({key}) => {
+        // 获取认识、不认识、下一个、撤销四个元素
         const know = document.querySelector('div[class*=index_green]')
         const dontKnow = document.querySelector('div[class*=index_red]')
         const next = document.querySelector('div[class*=StudyPage_nextBtn]')
         const undo = document.querySelector('div[class*=Message_message] div span')
 
+        // j: 如果页面上有 know 的元素，则触发其点击事件，没有的话就触发 next 的点击事件
+        // k: 同理
         if (key === 'j') click(know || next)
         if (key === 'k') click(dontKnow || undo)
     })
